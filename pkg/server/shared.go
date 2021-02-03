61ccaa74254cd56969119fee9627e1a17673f122
Gustavo Alpendere <gustavo.alpendere@nextroll.com>
2021-02-03 15:16:34 -0300

    OI

--- pkg/server/shared.go
+++ pkg/server/shared.go
@@ -7,8 +7,8 @@ import (
 
 func enableCors(w *http.ResponseWriter, r *http.Request) {
 	if strings.HasPrefix(r.Referer(), "https://guschnwg.github.io") {
-		(*w).Header().Set("Access-Control-Allow-Origin", "https://guschnwg.github.io/*")
+		(*w).Header().Set("Access-Control-Allow-Origin", "https://guschnwg.github.io")
 	} else if strings.HasPrefix(r.Referer(), "https://evening-ridge-00695.herokuapp.com") {
-		(*w).Header().Set("Access-Control-Allow-Origin", "https://evening-ridge-00695.herokuapp.com/*")
+		(*w).Header().Set("Access-Control-Allow-Origin", "https://evening-ridge-00695.herokuapp.com")
 	}
 }
