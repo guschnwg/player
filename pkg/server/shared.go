d00e4d3c4a4900a38dcddf094f13cd70e84aef52
Gustavo Alpendere <gustavo.alpendere@nextroll.com>
2021-02-03 15:16:15 -0300

    OI

--- pkg/server/shared.go
+++ pkg/server/shared.go
@@ -1,11 +1,14 @@
 package server
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func enableCors(w *http.ResponseWriter, r *http.Request) {
-	if r.Referer() == "https://guschnwg.github.io/" {
+	if strings.HasPrefix(r.Referer(), "https://guschnwg.github.io") {
 		(*w).Header().Set("Access-Control-Allow-Origin", "https://guschnwg.github.io/*")
-	} else if r.Referer() == "https://evening-ridge-00695.herokuapp.com/*" {
+	} else if strings.HasPrefix(r.Referer(), "https://evening-ridge-00695.herokuapp.com") {
 		(*w).Header().Set("Access-Control-Allow-Origin", "https://evening-ridge-00695.herokuapp.com/*")
 	}
 }
